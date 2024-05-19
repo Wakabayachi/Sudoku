1cc7a7ef603a1a047297d2e85b4bb272cabff269
Daniel Nastalski <maspa1987@gmail.com>
2024-05-19 23:37:13 +0200

    first commit

---
+++ src/main/java/com/kodilla/sudoku3/Sudoku3Application.java
@@ -0,0 +1,13 @@
+package com.kodilla.sudoku3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sudoku3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sudoku3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/sudoku3/Sudoku3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.sudoku3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sudoku3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
